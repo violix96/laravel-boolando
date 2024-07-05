825ddb60f5e1ab4797e380fae097899c9d06f629
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-07-05 20:46:44 +0200

    [UPD] integrated dynamic card display with badges and pricing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,15 +2,43 @@
 
 @section('content')
     <main>
-        <div class="container">
+        <div class="container mt-5">
             <div class="row">
-                <div class="col-33">
-                    @foreach ($comix as $comixs)
-                        <p>{{ $comixs['brand'] }}</p>
-                    @endforeach
-                </div>
+                @foreach ($comix as $product)
+                    <div class="col-33">
+                        <div class="content">
+                            <div>
+                                <img class="main-img" src="{{ Vite::asset('resources/img/' . $product['frontImage']) }}"
+                                    alt="{{ $product['name'] }}">
+                                <img class="hover-img" src="{{ Vite::asset('resources/img/' . $product['backImage']) }}"
+                                    alt="{{ $product['name'] }} back image">
+                                <div class="absolute">
+                                    @foreach ($product['badges'] as $badge)
+                                        @if ($badge['type'] == 'discount')
+                                            <span class="sconti-label">{{ $badge['value'] }}</span>
+                                        @elseif ($badge['type'] == 'tag')
+                                            <span class="sostenibilità-label">{{ $badge['value'] }}</span>
+                                        @endif
+                                    @endforeach
+                                </div>
+                                <div class="content-like">
+                                    <span class="cuoricino">&#9829;</span>
+                                </div>
+                            </div>
+                            <p>{{ $product['brand'] }}</p>
+                            <h4>{{ $product['name'] }}</h4>
+                            <span class="prezzo-ora">{{ $product['price'] }} &euro;</span>
+                            <span class="prezzo-prima">
+                                @foreach ($product['badges'] as $badge)
+                                    @if ($badge['type'] == 'discount')
+                                        {{ $badge['value'] }}
+                                    @endif
+                                @endforeach
+                            </span>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        </div>
         </div>
     </main>
 @endsection
