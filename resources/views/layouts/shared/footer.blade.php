3790979d1dda9bfbfc770dc58c16e8729f41cf44
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-07-05 21:28:23 +0200

    [UPD] added shared footer

--- resources/views/layouts/shared/footer.blade.php
+++ resources/views/layouts/shared/footer.blade.php
@@ -1,1 +1,28 @@
-<footer>footer</footer>
+<footer>
+    <div class="container">
+        <div class="row justify-between">
+            <div class="col-50">
+                <div>
+                    <h2>Boolando s.r.l.</h2>
+                    <a href="">Informazioni legali</a>
+                    <a href="">Informativa sulla privacy</a>
+                    <a href="">Diritto di recesso</a>
+                </div>
+            </div>
+            <div class="col-50">
+                <div>
+                    <h2>Trovaci anche su</h2>
+                    <img class="social-icon" src="{{ Vite::asset('resources/img/icons8-twitter.svg') }}" alt="twitter" />
+                    <img class="social-icon" src="{{ Vite::asset('resources/img/icons8-facebook.svg') }}"
+                        alt="facebook" />
+                    <img class="social-icon" src="{{ Vite::asset('resources/img/icons8-instagram.svg') }}"
+                        alt="instagram" />
+                    <img class="social-icon" src="{{ Vite::asset('resources/img/icons8-pinterest.svg') }}"
+                        alt="pinterest" />
+                    <img class="social-icon" src="{{ Vite::asset('resources/img/icons8-youtube.svg') }}"
+                        alt="youtube" />
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
