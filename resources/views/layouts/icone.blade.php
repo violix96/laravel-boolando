4f132b04d4de96c18e8340e522ba6c69bbb3e6cb
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-07-05 16:33:26 +0200

    [UPD] added header style in page

--- resources/views/layouts/icone.blade.php
+++ resources/views/layouts/icone.blade.php
@@ -2,17 +2,17 @@
     <ul>
         <li>
             <a href="">
-                < <img src="{{ Vite::asset('resources/img/like--v1.png') }}" alt="Logo">
+                <img class="icon" src="{{ Vite::asset('resources/img/like--v1.png') }}" alt="Logo">
             </a>
         </li>
         <li>
             <a href="">
-                < <img src="{{ Vite::asset('resources/img/shopping-bag.png') }}" alt="bag">
+                <img class="icon" src="{{ Vite::asset('resources/img/shopping-bag.png') }}" alt="bag">
             </a>
         </li>
         <li>
             <a href="">
-                < <img src="{{ Vite::asset('resources/img/user.png') }}" alt="user">
+                <img class="icon" src="{{ Vite::asset('resources/img/user.png') }}" alt="user">
             </a>
         </li>
     </ul>
